49b379b433f6f75d749d7907ddfccaaa63f8097b
zhiling <mai@dalaomai.cn>
2020-03-29 20:50:08 +0800

    fix code

--- hls_general.sh
+++ hls_general.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASEDIR="/root/test_ffmpeg"
+BASEDIR="/mnt/g_upenn"
 
 
 function hlsfile(){
@@ -20,10 +20,10 @@ function hlsfile(){
 			
 			if  ! cat ${m3u8file} | grep "#EXT-X-ENDLIST" &>/dev/null ; then
 				tmp_m3u8dir="/tmp/${m3u8dir}"
-				mkdir -p tmp_m3u8dir
+				mkdir -p ${tmp_m3u8dir}
 
 				ffmpeg -i "${file}" -c copy -bsf:v h264_mp4toannexb -hls_time 6 -hls_list_size 0 -hls_segment_filename "${tmp_m3u8dir}/%04d.ts" "${m3u8file}" > /dev/null 2>&1
-				mv tmp_m3u8dir m3u8dir
+				mv ${tmp_m3u8dir} ${m3u8dir}
 				echo "${file}  完成切片"
 			fi
 			
