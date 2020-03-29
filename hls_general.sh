4214ed946e197433a02e7025897521d0bf6e81ee
zhiling <mai@dalaomai.cn>
2020-03-29 20:55:20 +0800

    fix code

--- hls_general.sh
+++ hls_general.sh
@@ -19,10 +19,12 @@ function hlsfile(){
 
 			
 			if  ! cat ${m3u8file} | grep "#EXT-X-ENDLIST" &>/dev/null ; then
-				tmp_m3u8dir="/tmp/${m3u8dir}"
+				tmp_m3u8dir="/tmp${m3u8dir}"
+				tmp_m3u8file="/tmp${m3u8file}"
+				echo "临时文件：${tmp_m3u8dir}"
 				mkdir -p ${tmp_m3u8dir}
 
-				ffmpeg -i "${file}" -c copy -bsf:v h264_mp4toannexb -hls_time 6 -hls_list_size 0 -hls_segment_filename "${tmp_m3u8dir}/%04d.ts" "${m3u8file}" > /dev/null 2>&1
+				ffmpeg -i "${file}" -c copy -bsf:v h264_mp4toannexb -hls_time 6 -hls_list_size 0 -hls_segment_filename "${tmp_m3u8dir}/%04d.ts" "${tmp_m3u8file}" > /dev/null 2>&1
 				mv ${tmp_m3u8dir} ${m3u8dir}
 				echo "${file}  完成切片"
 			fi
