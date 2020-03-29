14c5b005e564596bb878145d62f3f3fac97aca48
zhiling <mai@dalaomai.cn>
2020-03-29 20:42:34 +0800

    优化 脚本

--- hls_general.sh
+++ hls_general.sh
@@ -19,7 +19,11 @@ function hlsfile(){
 
 			
 			if  ! cat ${m3u8file} | grep "#EXT-X-ENDLIST" &>/dev/null ; then
-				ffmpeg -i "${file}" -c copy -bsf:v h264_mp4toannexb -hls_time 6 -hls_list_size 0 -hls_segment_filename "${m3u8dir}/%04d.ts" "${m3u8file}" > /dev/null 2>&1
+				tmp_m3u8dir="/tmp/${m3u8dir}"
+				mkdir -p tmp_m3u8dir
+
+				ffmpeg -i "${file}" -c copy -bsf:v h264_mp4toannexb -hls_time 6 -hls_list_size 0 -hls_segment_filename "${tmp_m3u8dir}/%04d.ts" "${m3u8file}" > /dev/null 2>&1
+				mv tmp_m3u8dir m3u8dir
 				echo "${file}  完成切片"
 			fi
 			
